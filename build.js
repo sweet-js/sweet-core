7d9070ae39094e5bf849b31b37383cebf2149666
Tim Disney <tim.disney@gmail.com>
2012-09-16 15:36:42 -0700

    using shelljs instead of grunt

---
+++ build.js
@@ -0,0 +1,32 @@
+require("shelljs/make");
+var path = require("path");
+
+target.all = function() {
+  target.build();
+  target.build_browser();
+  target.test();
+}
+
+target.build = function() {
+  // move the compiler over to the lib dir...eventually should self-host
+  cp("-f", "src/*.js", "lib/");
+
+  if(!test('-d', 'build/')) {
+    mkdir("build/");
+  }
+
+  ls("test/*.js").forEach(function(file) {
+    echo("compiling: " + path.basename(file));
+    exec("bin/sjs --output build/" + path.basename(file) + " " + file);
+  });
+}
+
+target.build_browser = function() {
+  echo("building browser tests");
+  var test_files = ls("build/*.js").join(" ");
+  exec("browserify -o browser/test_bundled.js " + test_files);
+}
+
+target.test = function() {
+  exec("mocha --growl build/");
+}
