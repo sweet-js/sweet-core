2c74e4091a66f7db0455f91ffebb96b7e492d57f
Tim Disney <tim.disney@gmail.com>
2013-01-27 16:23:35 -0800

    whitespace cleaning

--- build.js
+++ build.js
@@ -4,62 +4,62 @@ var fs = require("fs");
 var Mocha = require("mocha");
 
 target.all = function() {
-  target.clean();
-  target.build();
-  target.build_test();
-  target.build_browser();
-  target.test();
+    target.clean();
+    target.build();
+    target.build_test();
+    target.build_browser();
+    target.test();
 }
 
 target.clean = function() {
-  rm("build/*");
+    rm("build/*");
 }
 
 target.single = function() {
-  target.build();
-  target.test_single();
+    target.build();
+    target.test_single();
 }
 
 target.build = function() {
-  // move the compiler over to the lib dir...eventually should self-host
-  if(!test('-d', 'build/')) {
-    mkdir("build/");
-  }
+    // move the compiler over to the lib dir...eventually should self-host
+    if(!test('-d', 'build/')) {
+        mkdir("build/");
+    }
 
-  cp("-f", "src/*.js", "lib/");
-  cp("-f", "test/test_single.js", "build/");
+    cp("-f", "src/*.js", "lib/");
+    cp("-f", "test/test_single.js", "build/");
 }
 
 target.build_test = function() {
 
-  ls("test/*.js").forEach(function(file) {
-    echo("compiling: " + path.basename(file));
-    exec("bin/sjs --output build/" + path.basename(file) + " " + file);
-  });
+    ls("test/*.js").forEach(function(file) {
+        echo("compiling: " + path.basename(file));
+        exec("bin/sjs --output build/" + path.basename(file) + " " + file);
+    });
 }
 
 target.build_browser = function() {
-  echo("\nbuilding browser tests...");
+    echo("\nbuilding browser tests...");
 
-  cp("-f", "lib/*.js", "browser/scripts");
+    cp("-f", "lib/*.js", "browser/scripts");
 }
 
 target.test = function() {
-  echo("\nrunning tests...");
-  var mocha = new Mocha();
+    echo("\nrunning tests...");
+    var mocha = new Mocha();
 
-  fs.readdirSync('build/').filter(function(file) {
-    return file.substr(-3) === '.js';
-  }).forEach(function(file) {
-    mocha.addFile(path.join("build/", file));
-  });
-  mocha.run();
+    fs.readdirSync('build/').filter(function(file) {
+        return file.substr(-3) === '.js';
+    }).forEach(function(file) {
+        mocha.addFile(path.join("build/", file));
+    });
+    mocha.run();
 }
 
 // used when we don't want to run all the tests again, just
 // run test_single.js
 target.test_single = function() {
-  var mocha = new Mocha();
-  mocha.addFile(path.join('build/', 'test_single.js'));
-  mocha.run();
+    var mocha = new Mocha();
+    mocha.addFile(path.join('build/', 'test_single.js'));
+    mocha.run();
 }
