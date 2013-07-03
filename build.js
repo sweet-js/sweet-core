523abea173a9049eb03e35678b02038c4cbaa2d4
Tim Disney <tim.disney@gmail.com>
2013-07-03 12:05:44 -0700

    separate build step for replacing sweet source after building

--- build.js
+++ build.js
@@ -30,7 +30,7 @@ target.run_bench = function() {
 
 target.clean = function() {
     if(test('-d', 'build/')) {
-        rm("build/*");
+        rm("-r", "build/");
     }
 };
 
@@ -45,28 +45,37 @@ target.unit = function() {
 };
 
 target.build = function() {
-    if(!test('-d', 'build/')) {
-        mkdir("build/");
+    if(!test('-d', "build/lib/")) {
+        mkdir("-p", "build/lib/");
+        mkdir("-p", "build/bin/");
     }
 
     echo("\nbuilding sweet.js...");
+
+    cp("-f", "bin/sjs", "build/bin/");
+    chmod("+x", "build/bin/sjs");
 
     ls("src/*.js").forEach(function(file) {
         echo("compiling: " + path.basename(file));
         // compile the expander only with support for contract macros
         if(file === "src/expander.js") {
-            exec("bin/sjs --output lib/" + path.basename(file) + " --module " + contracts_lib + " " + file);
+            exec("bin/sjs --output " + "build/lib/" + path.basename(file) + " --module " + contracts_lib + " " + file);
         } else {
-            exec("bin/sjs --output lib/" + path.basename(file) + " " + file);
+            exec("bin/sjs --output " + "build/lib/" + path.basename(file) + " " + file);
         }
     });
 };
+
+target.build_sweetjs = function() {
+    target.build();
+    cp("-f", "build/lib/*.js", "lib/");
+}
 
 target.build_test_file = function() {
     // if we have a "test.js" file sitting at the
     // root of the project go ahead and build it
     if(test('-f', "test.js")) {
-        exec('bin/sjs test.js test_out.js');
+        exec('build/bin/sjs test.js test_out.js');
     }
 }
 
@@ -74,7 +83,7 @@ target.build_test = function() {
 
     ls("test/*.js").forEach(function(file) {
         echo("compiling: " + path.basename(file));
-        exec("bin/sjs --output build/" + path.basename(file) + " " + file);
+        exec("build/bin/sjs --output build/" + path.basename(file) + " " + file);
     });
 };
 
---
+++ src/sjs.js
@@ -0,0 +1,49 @@
+var fs = require("fs");
+
+var sweet = require("./sweet.js");
+
+var argv = require("optimist")
+            .usage("Usage: sjs [options] path/to/file.js")
+            .demand(1)
+            .alias('o', 'output')
+            .describe('o', 'Output file path')
+            .alias('m', 'module')
+            .describe('m', 'use a module file for loading macro definitions')
+            .alias('w', 'watch')
+            .describe('w', 'watch a file')
+            .boolean('watch')
+            .alias('t', 'tokens')
+            .describe('t', 'just emit the expanded tokens without parsing an AST')
+            .argv;
+
+exports.run = function() {
+    var infile = argv._[0];
+    var outfile = argv.output;
+    var watch = argv.watch;
+    var tokens = argv.tokens;
+    var file = fs.readFileSync(infile, "utf8");
+    var module = argv.module;
+    var modulefile;
+
+    if(module) {
+        modulefile = fs.readFileSync(module, "utf8");
+        file = modulefile + "\n" + file;
+    }
+    
+	if (watch && outfile){
+		fs.watch(infile, function(){
+			file = fs.readFileSync(infile, "utf8");
+			try {
+				fs.writeFileSync(outfile, sweet.compile(file), "utf8");
+			} catch (e) {
+				console.log(e);
+			}
+		});
+	} else if(outfile) {
+       fs.writeFileSync(outfile, sweet.compile(file), "utf8");
+    } else if(tokens) {
+        console.log(sweet.expand(file))
+    } else {
+        console.log(sweet.compile(file));
+    }
+};
