ecaf7b08085dd3982148b07de38927ca48b4851d
Tim Disney <tim.disney@gmail.com>
2013-11-17 17:51:58 -0800

    macro test for sourcemaps

--- test/test_sourcemaps.js
+++ test/test_sourcemaps.js
@@ -44,11 +44,26 @@ describe "source mapping" {
 
     it "should work for multiple lines" {
         var pos = run("var x;\nvar y;", {
+            // var x;
+            // var y;
+            //     ^
             line: 2,
             column: 4
         });
 
         expect(pos.line).to.be(2);
         expect(pos.column).to.be(4);
+    }
+
+    it "should work with a simple macro" {
+        var pos = run("macro id { rule { $x } => { $x }}\nid 42;", {
+            // 42;
+            // ^
+            line: 1,
+            column:0
+        });
+
+        expect(pos.line).to.be(2);
+        expect(pos.column).to.be(0);
     }
 }
