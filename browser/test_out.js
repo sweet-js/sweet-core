a54f143bca709eb4e62bac7fae4cb87daead03b0
Tim Disney <tim.disney@gmail.com>
2013-11-17 17:53:20 -0800

    web pages to run sourcemap tests in

---
+++ browser/test_out.js
@@ -0,0 +1,10 @@
+function foo$111() {
+    var x1$112 = 1;
+    var x2$113 = 2;
+    var x3$114 = 3;
+    var x4$115 = 4;
+    var x5$116 = 5;
+    return x1$112 + x2$113 + x3$114 + x4$115 + x5$116;
+}
+console.log(foo$111());
+//# sourceMappingURL=test_out.js.map
