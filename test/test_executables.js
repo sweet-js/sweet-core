ddb173c524703c5f1f4093e882d93abd61010c7f
Austin King <shout@ozten.com>
2014-06-11 12:41:24 -0700

    Cleanup test

--- test/test_executables.js
+++ test/test_executables.js
@@ -2,11 +2,10 @@ var expect = require("expect.js");
 var path = require("path");
 
 describe('Loading executables does not die on HashBang', function() {
-
-  it('loads code from macros defined in imported module', function() {
-    process.argv = [ 'node', 'jsj', path.join(__dirname, './fixtures/test_require/executable.sjs')];
+  it('Have CLI copy of sjs.js load an executable', function() {
+    // Mock out arguments for optimist to load our fixture
+    process.argv = [ 'node', 'some/bin/sjs', path.join(__dirname, './fixtures/test_require/executable.sjs')];
     var sjs = require("../lib/sjs.js");
-    console.log(sjs);
 
     sjs.run();
   });
