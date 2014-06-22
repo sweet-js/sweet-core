ff9edbb0189a08c1649fd97569e1c1e8b2396f5f
Tim Disney <tim.disney@gmail.com>
2014-06-21 19:14:40 -0700

    build

--- browser/scripts/patterns.js
+++ browser/scripts/patterns.js
@@ -542,10 +542,12 @@
                 // pattern groups don't match the delimiters
                 subMatch = matchPatterns(pattern.inner, stx, context, true);
                 rest = subMatch.rest;
+                success = subMatch.success;
             } else if (pattern.class === 'named_group') {
                 subMatch = matchPatterns(pattern.inner, stx, context, true);
                 rest = subMatch.rest;
-                if (subMatch.success) {
+                success = subMatch.success;
+                if (success) {
                     var namedMatch = {};
                     namedMatch[pattern.value] = {
                         level: 0,
@@ -565,15 +567,12 @@
                 }
                 subMatch = matchPatterns(pattern.inner, stx[0].token.inner, context, false);
                 rest = stx.slice(1);
+                success = subMatch.success;
             } else {
-                return {
-                    success: false,
-                    rest: stx,
-                    patternEnv: patternEnv
-                };
+                subMatch = matchPatterns(pattern.inner, [], context, false);
+                success = false;
             }
-            success = subMatch.success;
-            if (success) {
+            if (success || !success && pattern.repeat) {
                 patternEnv = loadPatternEnv(patternEnv, subMatch.patternEnv, topLevel, pattern.repeat);
             }
         } else {
