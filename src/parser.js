73edcf0b7c104fa0abc27386a3de2c82402fc9df
Tim Disney <tim.disney@gmail.com>
2014-09-06 20:31:57 -0700

    cherry picking from esprima harmony branch
    commit 233179e192c20a7c60554cfe1080198ba3df9892

--- src/parser.js
+++ src/parser.js
@@ -1203,12 +1203,14 @@ parseYieldExpression: true
                     if (ch ===  '\r' && source[index] === '\n') {
                         ++index;
                     }
+                    lineStart = index;
                 }
             } else if (isLineTerminator(ch.charCodeAt(0))) {
                 ++lineNumber;
                 if (ch ===  '\r' && source[index] === '\n') {
                     ++index;
                 }
+                lineStart = index;
                 cooked += '\n';
             } else {
                 cooked += ch;
