ba049162ff2eb33052a5f2557c93bf77bed08668
Tim Disney <tim.disney@gmail.com>
2014-09-12 01:33:55 -0700

    cleaning up some more of the comment handling

--- src/parser.js
+++ src/parser.js
@@ -1509,6 +1509,12 @@ parseYieldExpression: true
 
         index = lookahead.range[0];
 
+        if (token.leadingComments) {
+            extra.comments = extra.comments.concat(token.leadingComments);
+            extra.trailingComments = extra.trailingComments.concat(token.leadingComments);
+            extra.leadingComments = extra.leadingComments.concat(token.leadingComments);
+        }
+
 
         return token;
     }
@@ -4337,7 +4343,7 @@ parseYieldExpression: true
     }
 
     function parseParams(firstRestricted) {
-        var options, marker = markerCreate();
+        var options;
 
         options = {
             params: [],
@@ -4365,7 +4371,7 @@ parseYieldExpression: true
             options.defaults = [];
         }
 
-        return markerApply(marker, options);
+        return options;
     }
 
     function parseFunctionDeclaration() {
@@ -5747,14 +5753,6 @@ parseYieldExpression: true
         extra.bottomRightStack = [];
         extra.trailingComments = [];
         extra.leadingComments = [];
-
-        tokenStream.forEach(function(stx) {
-            if (stx.token.leadingComments) {
-                extra.comments = extra.comments.concat(stx.token.leadingComments);
-                extra.trailingComments = extra.trailingComments.concat(stx.token.leadingComments);
-                extra.leadingComments = extra.leadingComments.concat(stx.token.leadingComments);
-            }
-        });
 
         if (typeof options !== 'undefined') {
             extra.range = (typeof options.range === 'boolean') && options.range;
