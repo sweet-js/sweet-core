bcd52c8651dd1cf4211ce2c6ed8c396a4bae3b83
Tim Disney <tim.disney@gmail.com>
2014-09-11 23:13:22 -0700

    updated comment handling to new esprima way

--- src/parser.js
+++ src/parser.js
@@ -5665,6 +5665,8 @@ parseYieldExpression: true
         
         extra = {};
         extra.comments = [];
+        extra.range = true;
+        extra.loc = true;
         patch();
 
         source = code;
@@ -5745,6 +5747,14 @@ parseYieldExpression: true
         extra.bottomRightStack = [];
         extra.trailingComments = [];
         extra.leadingComments = [];
+
+        tokenStream.forEach(function(stx) {
+            if (stx.token.leadingComments) {
+                extra.comments = extra.comments.concat(stx.token.leadingComments);
+                extra.trailingComments = extra.trailingComments.concat(stx.token.leadingComments);
+                extra.leadingComments = extra.leadingComments.concat(stx.token.leadingComments);
+            }
+        });
 
         if (typeof options !== 'undefined') {
             extra.range = (typeof options.range === 'boolean') && options.range;
