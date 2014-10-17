54a1a6a2ac05ce9541114fa0d6313f9b43b0166e
VimalKumarS <vimal.kumar@sjsu.edu>
2014-10-16 17:13:46 -0700

    removed file

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
