1c3dd0a7e4524e8995a264cd22675f10a6eafc0e
Paul Taylor <ptaylor@netflix.com>
2014-12-28 09:12:54 -0500

    Fixes issue #174. Instead of failing hard on empty repeats, bail early.

--- src/patterns.js
+++ src/patterns.js
@@ -919,8 +919,12 @@
                             return env[pat].level > 0;
                         });
 
-                        assert(typeof nonScalar !== 'undefined',
-                                      "must have a least one non-scalar in repeat");
+                        if(typeof nonScalar === 'undefined') {
+                            return acc;
+                        }
+
+                        // assert(typeof nonScalar !== 'undefined',
+                        //               "must have a least one non-scalar in repeat");
 
                         var repeatLength = env[nonScalar].match.length;
                         var sameLength = _.all(fv, function(pat) {
