d5490dbbcf3ddf7b2f5c49535b4c3ace61bd0897
Christopher Schuster <cs@livoris.de>
2015-02-03 16:41:58 -0800

    more tests for repeated vars (groups, bindings, ellipses)

--- browser/scripts/reverse.js
+++ browser/scripts/reverse.js
@@ -1,27 +1,3 @@
-/*
-  Copyright (C) 2015 Tim Disney <tim@disnet.me>
-
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 (function (root, factory) {
     if (typeof exports === 'object') {
         // CommonJS
@@ -48,11 +24,6 @@
     var expandN = _.initial(parser.read('$n'));
     var expandP = _.initial(parser.read('$p...'));
     var expandE = _.initial(parser.read('$e...'));
-    // Pre-order traversal of read tree, provides rest tokens at each step
-    //
-    // fn :: a -> Token -> a
-    // stx :: [Token] (can be nested)
-    // initial :: a
     function foldReadTree(fn, stx, initial, path) {
         var current = initial;
         if (!path) {
@@ -72,7 +43,6 @@
         path.pop();
         return current;
     }
-    // create a new tree by walking the path up and replacing nodes
     function replaceInTree(newStx, path) {
         if (path.length === 0) {
             return newStx;
