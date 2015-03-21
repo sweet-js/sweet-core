1e2d7f2d5f7c51c92174ba3278f3554c0fb6b229
Tim Disney <tim.disney@gmail.com>
2015-03-21 16:31:03 -0700

    to5 at the moment

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
             dev: {
                 options: {
                     sourceMap: false,
-                    babel: true,
+                    to5: true,
                     compileFrom: "./lib/sweet",
                 },
                 files: [{
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
             },
             release: {
                 options: {
-                    babel: true,
+                    to5: true,
                     compileFrom: "./lib/sweet"
                 },
                 files: [{
@@ -75,7 +75,7 @@ module.exports = function(grunt) {
             single: {
                 options: {
                     sourceMap: false,
-                    babel: true,
+                    to5: true,
                     readableNames: false,
                     compileFrom: "./build/lib/sweet"
                 },
@@ -278,7 +278,7 @@ module.exports = function(grunt) {
                 var output = sweet.compile(code, {
                     sourceMap: options.sourceMap,
                     filename: file,
-                    babel: options.babel,
+                    to5: options.to5,
                     readableNames: options.readableNames,
                     modules: modules
                 })[0];
