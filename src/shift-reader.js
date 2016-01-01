863083513fea11254fb15014ea2bf21b135081a1
Tim Disney <tim.disney@gmail.com>
2015-12-31 21:16:26 -0800

    Add some regex disambig

--- src/shift-reader.js
+++ src/shift-reader.js
@@ -2,12 +2,27 @@ import Tokenizer from "shift-parser/dist/tokenizer";
 import { TokenClass, TokenType } from "shift-parser/dist/tokenizer";
 import { List } from "immutable";
 import Syntax from "./syntax";
+import * as R from 'ramda';
+
+let isRegexPrefix = R.anyPass([
+  R.whereEq({ size: 0 }),
+  p => p.last() && p.last().isPunctuator(),
+  p => {
+    let last = p.last();
+    if (last.isKeyword() && p.pop().size !== 0) {
+      let last = p.pop().last();
+      return last.isPunctuator() && last.val() !== '.';
+    }
+    return last.isKeyword();
+  }
+]);
 
 // TODO: port regex disambiguation from old reader
 export default class Reader extends Tokenizer.default {
   constructor(source) {
     super(source);
     this.delimStack = new Map();
+    this.prefix = List();
   }
 
   // (?[Syntax]) -> List<Syntax>
@@ -23,16 +38,30 @@ export default class Reader extends Tokenizer.default {
           tok.type === TokenType.LBRACK ||
           tok.type === TokenType.LBRACE) {
         let inner = this.read([new Syntax(tok)]);
-        stack.push(new Syntax(inner));
+        let stx = new Syntax(inner);
+        this.prefix = this.prefix.concat(stx);
+        stack.push(stx);
       } else if (tok.type === TokenType.RPAREN ||
                  tok.type === TokenType.RBRACK ||
                  tok.type === TokenType.RBRACE) {
-        stack.push(new Syntax(tok));
+        let stx = new Syntax(tok);
+        this.prefix = this.prefix.concat(stx);
+        stack.push(stx);
         break;
       } else {
-        stack.push(new Syntax(tok));
+        let stx = new Syntax(tok);
+        this.prefix = this.prefix.concat(stx);
+        stack.push(stx);
       }
     }
     return List(stack);
+  }
+
+  advance() {
+    let lookahead = super.advance();
+    if (lookahead.type === TokenType.DIV && isRegexPrefix(this.prefix)) {
+      return super.scanRegExp("/");
+    }
+    return lookahead;
   }
 }
