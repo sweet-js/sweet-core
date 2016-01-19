edd047771a2c16b9656dfd63bdd717dc09964537
Tim Disney <tim.disney@gmail.com>
2016-01-19 07:48:30 +0000

    wip

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-syntax # = function(ctx) {
-  let arg = ctx.next();
-  return syntaxQuote`syntaxQuote${arg}`;
-}
-
-syntax m = function(ctx) {
-  let x = ctx.nextExpression();
-  return #`40 + ${x}`;
-}
-m 2;
+for ( ;;);
