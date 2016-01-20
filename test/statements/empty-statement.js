f059698adde9016826982c69353f49e0a2e47119
Tim Disney <tim.disney@gmail.com>
2016-01-19 23:18:32 -0800

    Add tests for empty

--- test/statements/empty-statement.js
+++ test/statements/empty-statement.js
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-var testParse = require("../assertions").testParse;
-var stmt = require("../helpers").stmt;
+import expect from "expect.js";
+import { expr, stmt, testParse, testParseFailure } from "./assertions";
 
-suite("Parser", function () {
-  suite("empty expression", function () {
+describe("Parser", function () {
+  it("empty expression", function () {
 
     testParse(";", stmt, { type: "EmptyStatement" });
 
