5c3b36a9f41d6178027155734d04afa9582a213b
Tim Disney <tim.disney@gmail.com>
2016-02-07 15:35:55 -0800

    Add more tests

--- test/declaration/function-declaration.js
+++ test/declaration/function-declaration.js
@@ -228,96 +228,96 @@ describe("Parser", function () {
     testParseFailure("a: function* a(){}", "Unexpected token \"*\"");
   });
 
-  // it("Annex B 3.4: function declarations in if statements", function () {
-  //
-  //   testParse("if (0) function a(){}", stmt, {
-  //     type: "IfStatement",
-  //     test: { type: "LiteralNumericExpression", value: 0 },
-  //     consequent: {
-  //       type: "FunctionDeclaration",
-  //       isGenerator: false,
-  //       name: { type: "BindingIdentifier", name: "a" },
-  //       params: { type: "FormalParameters", items: [], rest: null },
-  //       body: { type: "FunctionBody", directives: [], statements: [] }
-  //     },
-  //     alternate: null
-  //   });
-  //
-  //   testParse("if (0) function a(){} else;", stmt, {
-  //     type: "IfStatement",
-  //     test: { type: "LiteralNumericExpression", value: 0 },
-  //     consequent: {
-  //       type: "FunctionDeclaration",
-  //       isGenerator: false,
-  //       name: { type: "BindingIdentifier", name: "a" },
-  //       params: { type: "FormalParameters", items: [], rest: null },
-  //       body: { type: "FunctionBody", directives: [], statements: [] }
-  //     },
-  //     alternate: { type: "EmptyStatement" }
-  //   });
-  //
-  //   testParse("if (0); else function a(){}", stmt, {
-  //     type: "IfStatement",
-  //     test: { type: "LiteralNumericExpression", value: 0 },
-  //     consequent: { type: "EmptyStatement" },
-  //     alternate: {
-  //       type: "FunctionDeclaration",
-  //       isGenerator: false,
-  //       name: { type: "BindingIdentifier", name: "a" },
-  //       params: { type: "FormalParameters", items: [], rest: null },
-  //       body: { type: "FunctionBody", directives: [], statements: [] }
-  //     }
-  //   });
-  //
-  //   testParse("if (0) function a(){} else function b(){}", stmt, {
-  //     type: "IfStatement",
-  //     test: { type: "LiteralNumericExpression", value: 0 },
-  //     consequent: {
-  //       type: "FunctionDeclaration",
-  //       isGenerator: false,
-  //       name: { type: "BindingIdentifier", name: "a" },
-  //       params: { type: "FormalParameters", items: [], rest: null },
-  //       body: { type: "FunctionBody", directives: [], statements: [] }
-  //     },
-  //     alternate: {
-  //       type: "FunctionDeclaration",
-  //       isGenerator: false,
-  //       name: { type: "BindingIdentifier", name: "b" },
-  //       params: { type: "FormalParameters", items: [], rest: null },
-  //       body: { type: "FunctionBody", directives: [], statements: [] }
-  //     }
-  //   });
-  //
-  //   testParse("try {} catch (e) { if(0) function e(){} }", stmt, {
-  //     type: "TryCatchStatement",
-  //     body: { type: "Block", statements: [] },
-  //     catchClause: {
-  //       type: "CatchClause",
-  //       binding: { name: "e", type: "BindingIdentifier" },
-  //       body: {
-  //         type: "Block",
-  //         statements: [
-  //           {
-  //             type: "IfStatement",
-  //             test: { type: "LiteralNumericExpression", value: 0 },
-  //             consequent: {
-  //               type: "FunctionDeclaration",
-  //               isGenerator: false,
-  //               name: { type: "BindingIdentifier", name: "e" },
-  //               body: { type: "FunctionBody", directives: [], statements: [] },
-  //               params: { type: "FormalParameters", items: [], rest: null }
-  //             },
-  //             alternate: null
-  //           }
-  //         ]
-  //       }
-  //     }
-  //   });
-  //
+  it("Annex B 3.4: function declarations in if statements", function () {
+
+    testParse("if (0) function a(){}", stmt, {
+      type: "IfStatement",
+      test: { type: "LiteralNumericExpression", value: 0 },
+      consequent: {
+        type: "FunctionDeclaration",
+        isGenerator: false,
+        name: { type: "BindingIdentifier", name: "a" },
+        params: { type: "FormalParameters", items: [], rest: null },
+        body: { type: "FunctionBody", directives: [], statements: [] }
+      },
+      alternate: null
+    });
+
+    testParse("if (0) function a(){} else;", stmt, {
+      type: "IfStatement",
+      test: { type: "LiteralNumericExpression", value: 0 },
+      consequent: {
+        type: "FunctionDeclaration",
+        isGenerator: false,
+        name: { type: "BindingIdentifier", name: "a" },
+        params: { type: "FormalParameters", items: [], rest: null },
+        body: { type: "FunctionBody", directives: [], statements: [] }
+      },
+      alternate: { type: "EmptyStatement" }
+    });
+
+    testParse("if (0); else function a(){}", stmt, {
+      type: "IfStatement",
+      test: { type: "LiteralNumericExpression", value: 0 },
+      consequent: { type: "EmptyStatement" },
+      alternate: {
+        type: "FunctionDeclaration",
+        isGenerator: false,
+        name: { type: "BindingIdentifier", name: "a" },
+        params: { type: "FormalParameters", items: [], rest: null },
+        body: { type: "FunctionBody", directives: [], statements: [] }
+      }
+    });
+
+    testParse("if (0) function a(){} else function b(){}", stmt, {
+      type: "IfStatement",
+      test: { type: "LiteralNumericExpression", value: 0 },
+      consequent: {
+        type: "FunctionDeclaration",
+        isGenerator: false,
+        name: { type: "BindingIdentifier", name: "a" },
+        params: { type: "FormalParameters", items: [], rest: null },
+        body: { type: "FunctionBody", directives: [], statements: [] }
+      },
+      alternate: {
+        type: "FunctionDeclaration",
+        isGenerator: false,
+        name: { type: "BindingIdentifier", name: "b" },
+        params: { type: "FormalParameters", items: [], rest: null },
+        body: { type: "FunctionBody", directives: [], statements: [] }
+      }
+    });
+
+    testParse("try {} catch (e) { if(0) function e(){} }", stmt, {
+      type: "TryCatchStatement",
+      body: { type: "Block", statements: [] },
+      catchClause: {
+        type: "CatchClause",
+        binding: { name: "e", type: "BindingIdentifier" },
+        body: {
+          type: "Block",
+          statements: [
+            {
+              type: "IfStatement",
+              test: { type: "LiteralNumericExpression", value: 0 },
+              consequent: {
+                type: "FunctionDeclaration",
+                isGenerator: false,
+                name: { type: "BindingIdentifier", name: "e" },
+                body: { type: "FunctionBody", directives: [], statements: [] },
+                params: { type: "FormalParameters", items: [], rest: null }
+              },
+              alternate: null
+            }
+          ]
+        }
+      }
+    });
+
   //   testParseFailure("for(;;) function a(){}", "Unexpected token \"function\"");
   //   testParseFailure("for(a in b) function c(){}", "Unexpected token \"function\"");
   //   testParseFailure("for(a of b) function c(){}", "Unexpected token \"function\"");
   //   testParseFailure("while(true) function a(){}", "Unexpected token \"function\"");
   //   testParseFailure("with(true) function a(){}", "Unexpected token \"function\"");
-  // });
+  });
 });
