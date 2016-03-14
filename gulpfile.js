bb055413b4479e6faa8c63a262d1d7799efd6b0c
Tim Disney <tim.disney@gmail.com>
2016-03-13 21:47:10 -0700

    Clean build

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,9 @@
 var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
-var mocha = require('gulp-mocha');
 var webpack = require('gulp-webpack');
 
 var srcFiles = 'src/**/*.js';
-
-var testFiles = [
-  'test/*.js',
-  'test/declaration/*.js',
-  'test/statements/*',
-  'test/expressions/literals/*',
-  'test/expressions/*',
-
-  'test/modules/export.js',
-  'test/modules/import.js',
-
-  'test/destructuring/assignment/*',
-  'test/destructuring/binding-pattern/arr*',
-  'test/destructuring/binding-pattern/obj*',
-
-  '!test/miscellaneous/*',
-  '!test/property-definition/*'
-];
 
 gulp.task('build:src', function () {
   return gulp.src(srcFiles)
@@ -32,14 +13,6 @@ gulp.task('build:src', function () {
     .pipe(gulp.dest("build/src/"));
 });
 
-// gulp.task('build:test', function () {
-//   return gulp.src(testFiles)
-//     .pipe(sourcemaps.init())
-//     .pipe(babel())
-//     .pipe(sourcemaps.write("."))
-//     .pipe(gulp.dest("build/test/"));
-// });
-//
 gulp.task('build:browser', ['dist'], function () {
   return gulp.src('dist/sweet.js')
     .pipe(sourcemaps.init())
@@ -54,24 +27,7 @@ gulp.task('build:browser', ['dist'], function () {
     .pipe(gulp.dest("browser/scripts/"));
 });
 
-gulp.task('build', ['build:src']);
-
-// gulp.task('mocha', ['build:src', 'build:test'], function () {
-//   return gulp.src('build/test/**/*.js', { read: false })
-//     .pipe(mocha({
-//       reporter: 'nyan'
-//     }));
-// });
-//
-// gulp.task('mocha:single', ['build:src', 'build:test'], function () {
-//   return gulp.src('build/test/**/test_modules.js', { read: false })
-//     .pipe(mocha({
-//       reporter: 'nyan'
-//     }));
-// });
-
-
-gulp.task('default', ['build']);
+gulp.task('build', ['build:src', 'build:browser']);
 
 gulp.task("dist", function () {
   return gulp.src(srcFiles)
@@ -81,10 +37,4 @@ gulp.task("dist", function () {
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task('watch', ['default'], function () {
-  gulp.watch(['src/**/*.js', 'test/**/*.js'], ['default']);
-});
-
-gulp.task('watch:build', ['build:src'], function () {
-  gulp.watch(['src/**/*.js',], ['build:src']);
-});
+gulp.task('default', ['build']);
