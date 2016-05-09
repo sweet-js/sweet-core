9db7984da9ef5e5ae97a1c92b41e8497b8267dce
Tim Disney <tim.disney@gmail.com>
2016-05-08 17:09:11 -0700

    Use right constructor in setLineNumber

--- src/syntax.js
+++ src/syntax.js
@@ -203,7 +203,7 @@ export default class Syntax {
       assert(newTok.slice && newTok.slice.startLocation, 'all tokens must have line info');
       newTok.slice.startLocation.line = line;
     }
-    return new Syntax(newTok, this.context);
+    return new Syntax(newTok, this);
   }
 
   // () -> List<Syntax>
