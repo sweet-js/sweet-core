7667f610cc3b2b8218f57caf2a30d7b9b20d0aab
Tim Disney <tim.disney@gmail.com>
2016-06-12 21:15:26 -0700

    build

--- dist/scope.js
+++ dist/scope.js
@@ -9,17 +9,17 @@ var _errors = require("./errors");
 
 var _symbol = require("./symbol");
 
-let scopeIndex_578 = 0;
-function freshScope_579() {
-  let name_581 = arguments.length <= 0 || arguments[0] === undefined ? "scope" : arguments[0];
+let scopeIndex_579 = 0;
+function freshScope_580() {
+  let name_582 = arguments.length <= 0 || arguments[0] === undefined ? "scope" : arguments[0];
 
-  scopeIndex_578++;
-  return (0, _symbol.Symbol)(name_581 + "_" + scopeIndex_578);
+  scopeIndex_579++;
+  return (0, _symbol.Symbol)(name_582 + "_" + scopeIndex_579);
 }
 ;
-function Scope_580(name_582) {
-  return (0, _symbol.Symbol)(name_582);
+function Scope_581(name_583) {
+  return (0, _symbol.Symbol)(name_583);
 }
-exports.freshScope = freshScope_579;
-exports.Scope = Scope_580;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3N3ZWV0L3Njb3BlLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQTs7QUFDQTs7QUFDQSxJQUFJLGlCQUFpQixDQUFyQjtBQUNBLFNBQVMsY0FBVCxHQUE0QztBQUFBLE1BQXBCLFFBQW9CLHlEQUFULE9BQVM7O0FBQzFDO0FBQ0EsU0FBTyxvQkFBTyxXQUFXLEdBQVgsR0FBaUIsY0FBeEIsQ0FBUDtBQUNEO0FBQ0Q7QUFDQSxTQUFTLFNBQVQsQ0FBbUIsUUFBbkIsRUFBNkI7QUFDM0IsU0FBTyxvQkFBTyxRQUFQLENBQVA7QUFDRDtRQUN5QixVLEdBQWxCLGM7UUFDYSxLLEdBQWIsUyIsImZpbGUiOiJzY29wZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7ZXhwZWN0LCBhc3NlcnR9IGZyb20gXCIuL2Vycm9yc1wiO1xuaW1wb3J0IHtTeW1ib2wsIGdlbnN5bX0gZnJvbSBcIi4vc3ltYm9sXCI7XG5sZXQgc2NvcGVJbmRleF81NzggPSAwO1xuZnVuY3Rpb24gZnJlc2hTY29wZV81NzkobmFtZV81ODEgPSBcInNjb3BlXCIpIHtcbiAgc2NvcGVJbmRleF81NzgrKztcbiAgcmV0dXJuIFN5bWJvbChuYW1lXzU4MSArIFwiX1wiICsgc2NvcGVJbmRleF81NzgpO1xufVxuO1xuZnVuY3Rpb24gU2NvcGVfNTgwKG5hbWVfNTgyKSB7XG4gIHJldHVybiBTeW1ib2wobmFtZV81ODIpO1xufVxuZXhwb3J0IHtmcmVzaFNjb3BlXzU3OSBhcyBmcmVzaFNjb3BlfTtcbmV4cG9ydCB7U2NvcGVfNTgwIGFzIFNjb3BlfSJdfQ==
+exports.freshScope = freshScope_580;
+exports.Scope = Scope_581;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3N3ZWV0L3Njb3BlLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQTs7QUFDQTs7QUFDQSxJQUFJLGlCQUFpQixDQUFyQjtBQUNBLFNBQVMsY0FBVCxHQUE0QztBQUFBLE1BQXBCLFFBQW9CLHlEQUFULE9BQVM7O0FBQzFDO0FBQ0EsU0FBTyxvQkFBTyxXQUFXLEdBQVgsR0FBaUIsY0FBeEIsQ0FBUDtBQUNEO0FBQ0Q7QUFDQSxTQUFTLFNBQVQsQ0FBbUIsUUFBbkIsRUFBNkI7QUFDM0IsU0FBTyxvQkFBTyxRQUFQLENBQVA7QUFDRDtRQUN5QixVLEdBQWxCLGM7UUFDYSxLLEdBQWIsUyIsImZpbGUiOiJzY29wZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7ZXhwZWN0LCBhc3NlcnR9IGZyb20gXCIuL2Vycm9yc1wiO1xuaW1wb3J0IHtTeW1ib2wsIGdlbnN5bX0gZnJvbSBcIi4vc3ltYm9sXCI7XG5sZXQgc2NvcGVJbmRleF81NzkgPSAwO1xuZnVuY3Rpb24gZnJlc2hTY29wZV81ODAobmFtZV81ODIgPSBcInNjb3BlXCIpIHtcbiAgc2NvcGVJbmRleF81NzkrKztcbiAgcmV0dXJuIFN5bWJvbChuYW1lXzU4MiArIFwiX1wiICsgc2NvcGVJbmRleF81NzkpO1xufVxuO1xuZnVuY3Rpb24gU2NvcGVfNTgxKG5hbWVfNTgzKSB7XG4gIHJldHVybiBTeW1ib2wobmFtZV81ODMpO1xufVxuZXhwb3J0IHtmcmVzaFNjb3BlXzU4MCBhcyBmcmVzaFNjb3BlfTtcbmV4cG9ydCB7U2NvcGVfNTgxIGFzIFNjb3BlfSJdfQ==
