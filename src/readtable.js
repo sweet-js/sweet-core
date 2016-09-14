6b9b26b22e37a5db58cfd32c14e70faa2d3f2252
Gabe Johnson <gijohnson105@gmail.com>
2016-09-14 10:40:54 -0500

    Add Readtable

---
+++ src/readtable.js
@@ -0,0 +1,105 @@
+// @flow
+import { List } from 'immutable';
+import type CharStream from './char-stream';
+
+/*
+ * Note: these are possible features for future enhancements. 
+ * extendReadtable features:
+ * 1. { key, mode: TerminatingMacro, action } - creates a delimiter must return an Array/List
+ * 2. { key, mode: NonTerminatingMacro, action } - must return a Token or null/undefined. null/undefined simply consumes the read charstream.
+ * 3. { key, mode: DispatchMacro, action } - triggered by reading #. otherwise like 2
+ * 4. { key, delegateKey, delegateReadtable } - delegates to likeChar entry in readtable. Can be implemented
+ *    by getEntry(delegateReadtable, delegateKey), adding key and passing to extendReadtable
+ * 5. { key: null, mode: NonTerminatingMacro, action } - sets the default behavior for unmatched characters (identifiers/numbers)
+ */
+
+export default class Readtable {
+  _entries: Array<ReadtableEntry>;
+  constructor(entries: Array<ReadtableEntry> = []) {
+    this._entries = entries;
+  }
+  
+  getEntry(key: ReadtableKey): ReadtableEntry {
+    if (!isValidKey(key)) throw Error('Invalid key type:', key);
+    return this._entries[convertKey(key)] || this._entries[0];
+  }
+
+  extendReadtable(...entries: Array<ReadtableEntry>): Readtable {
+    const newTable = this._entries.slice();
+    return new Readtable(entries.reduce(addEntry, newTable));
+  }
+}
+
+function addEntry(table: Array<ReadtableEntry>, { key, action }: ReadtableEntry): Array<ReadtableEntry> {
+  if (!isValidKey(key)) throw Error('Invalid key type:', key);
+  if (!isValidEntry({key, action})) throw Error('Invalid readtable entry:', {key, action});
+
+  // null/undefined key is the default and will be converted to 0
+  // chars will be converted via codePointAt
+  // numbers are...numbers
+  // to accomodate default (null) 1 will be added to all and default will be at 0
+  table[convertKey(key)] = { action };
+
+  // if is a dispatch macro, we have to convert the key and bump it up by 0x110000
+  // Note: The above depends on a primitive implementation of dispatch macros.
+  //       I'm considering another implementation which will just use the current capabilities.
+  return table;
+}
+
+// TODO: do we even need this?
+export function isReadtable(readtable: any): boolean {
+  return readtable instanceof Readtable;
+}
+
+export const EmptyReadtable = new Readtable();
+
+// Note: This was an experiment. IF the mode flags are included here, they can just be string constants.
+// Symbol.for creates cross-realm Symbols
+// export const TerminatingMacro = Symbol.for('readtable.terminating-macro-mode');
+// export const NonTerminatingMacro = Symbol.for('readtable.non-terminating-macro-mode');
+// export const DispatchMacro = Symbol.for('readtable.dispatch-macro-mode');
+
+function isValidKey(key) {
+  return key == null ||
+    (typeof key === 'number' && key <= 0x10FFFF) ||
+    // TODO: not sure this is right
+    (typeof key === 'string' && (key.length === 1 || key.length === 2));
+}
+
+// function isValidMode(mode: Symbol): boolean {
+//   return mode === TerminatingMacro || mode === NonTerminatingMacro || mode === DispatchMacro;
+// }
+
+function isValidAction(action) {
+  return typeof action === "function";
+}
+
+type ReadtableKey = null | string | number;
+
+// type ReadtableMode = Symbol;
+
+type Token = { type: string, value: string };
+
+export type TokenTree = Token | List<TokenTree> | Array<TokenTree>;
+
+type Action = (stream : CharStream, prefix?: TokenTree) => TokenTree;
+
+export type ReadtableEntry = {
+  key?: ?ReadtableKey,
+  action: Action
+};
+
+function isValidEntry(entry) {
+  return entry && isValidKey(entry.key) && /*isValidMode(entry.mode) &&*/ isValidAction(entry.action);
+}
+
+function convertKey(key?: ReadtableKey): number {
+  return key == null ? 0 : (typeof key === 'number' ? key : key.codePointAt(0)) + 1;
+}
+
+// TODO: probably going to change this. It should be in the reader, or whatever uses the readtables.
+export let CurrentReadtable = null;
+
+export function setCurrentReadtable(readtable: Readtable): void {
+  CurrentReadtable = readtable;
+}
---
+++ test/test_readtable.js
@@ -0,0 +1,44 @@
+import { isReadtable,
+         EmptyReadtable } from '../src/readtable';
+
+import test from 'ava';
+
+const entry = {
+  key: '@',
+  action() {
+    return {
+      type: 'Punctuator',
+      value: 'AT'
+    };
+  }
+};
+
+let table = EmptyReadtable.extendReadtable(entry);
+
+test('extendReadtable should extend a readtable with new entries', t => {
+  t.true(isReadtable(table));
+});
+
+test('getEntry retrieves an entry from a readtable', t => {
+  const theEntry = table.getEntry('@');
+
+  t.is(theEntry.action, entry.action);
+});
+test('should be able to get an entry after a readtable has been extended', t => {
+  const newEntry = {
+    key: '#',
+    action() {
+      return {
+        type: 'Punctuator',
+        value: 'HASH'
+      }
+    }
+  };
+  const newTable = table.extendReadtable(newEntry);
+
+  const theEntry = newTable.getEntry('@');
+  t.is(theEntry.action, entry.action);
+
+  const theOtherEntry = newTable.getEntry('#');
+  t.is(theOtherEntry.action, newEntry.action);
+});
