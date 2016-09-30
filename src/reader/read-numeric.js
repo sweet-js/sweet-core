a61af50b47b8fe2afa2c40451aa7d0e7555743ff
Gabe Johnson <gijohnson105@gmail.com>
2016-09-29 21:45:19 -0500

    Reading numbers and strings.

---
+++ src/reader/read-numeric.js
@@ -0,0 +1,215 @@
+import { isEOS } from '../char-stream';
+import { code } from 'esutils';
+import { getHexValue } from './utils';
+
+const { isIdentifierPartES6: isIdentifierPart,
+        isIdentifierStartES6: isIdentifierStart,
+        } = code;
+
+export default function readNumericLiteral(stream) {
+  let idx = 0, char = stream.peek();
+
+  if (char === '0') {
+    char = stream.peek(1);
+    if (!isEOS(char)) {
+      char = char.toLowerCase();
+      switch (char) {
+        case 'x': return readHexLiteral(stream);
+        case 'b': return readBinaryLiteral(stream);
+        case 'o': return readOctalLiteral(stream);
+        default: if ('0' <= char && char <= '9') {
+          return readLegacyOctalLiteral(stream); // reads legacy octal and decimal
+        }
+      }
+    } else {
+      return {
+        type: 'NumericLiteral',
+        value: stream.readString(),
+        octal: false,
+        noctal: false
+      };
+    }
+  } else if (char !== '.') {
+    while (isDecimalChar(char)) {
+      ++idx;
+      char = stream.peek(idx);
+      if (isEOS(char)) {
+        return {
+          type: 'NumericLiteral',
+          value: stream.readString(idx),
+          octal: false,
+          noctal: false
+        }
+      }
+    }
+  }
+
+  idx = addDecimalLiteralSuffixLength(stream, idx);
+
+  char = stream.peek(++idx);
+  if (!isEOS(char) && isIdentifierStart(char)) {
+    throw Error('Illegal numeric literal');
+  }
+
+  return {
+    type: 'NumericLiteral',
+    value: +stream.readString(idx),
+    octal: false,
+    noctal: false
+  }
+}
+
+function addDecimalLiteralSuffixLength(stream, idx) {
+  let char = stream.peek(idx);
+  if (char === '.') {
+    char = stream.peek(++idx);
+    if (isEOS(char)) return idx - 1;
+
+    while (isDecimalChar(char)) {
+      char = stream.peek(++idx);
+      if (isEOS(char)) return idx - 1;
+    }
+  }
+
+  if (char.toLowerCase() === 'e') {
+    char = stream.peek(++idx);
+    if (isEOS(char)) throw Error('Illegal decimal literal suffix');
+
+    if (char === '+' || char === '-') {
+      char = stream.peek(++idx);
+      if (isEOS(char)) throw Error('Illegal decimal literal suffix');
+    }
+
+    if (isDecimalChar(char)) {
+      while (isDecimalChar(char)) {
+        char = stream.peek(++idx);
+        if (isEOS(char)) {
+          char = stream.peek(++idx);
+          if (isEOS(char)) break;
+        }
+      }
+    }
+  }
+  return idx;
+}
+
+function readLegacyOctalLiteral(stream) {
+  let idx = 0, isOctal = true, char = stream.peek();
+
+
+  while (!isEOS(char)) {
+    if ("0" <= char && char <= "7") {
+      idx++;
+    } else if (char === "8" || char === "9") {
+      isOctal = false;
+      idx++;
+    } else if (isIdentifierPart(char.charCodeAt(0))) {
+      throw Error("Illegal numeric literal");
+    } else {
+      break;
+    }
+
+    char = stream.peek(idx);
+  }
+
+  if (!isOctal) return {
+    type: 'NumericLiteral',
+    value: parseNumeric(stream, idx, 10),
+    octal: true,
+    noctal: !isOctal
+  };
+
+  return {
+    type: 'NumericLiteral',
+    value: parseNumeric(stream, idx, 8),
+    octal: true,
+    noctal: !isOctal
+  }
+}
+
+function readOctalLiteral(stream) {
+  let start, idx = start = 2, char = stream.peek(idx);
+  while (!isEOS(char)) {
+    if ("0" <= char && char <= "7") {
+      ++idx;
+      char = stream.peek(idx);
+    } else if (isIdentifierPart(char.charCodeAt(0))) {
+      throw Error("Illegal octal literal");
+    } else {
+      break;
+    }
+  }
+
+  if (idx === start) {
+    throw Error("Illegal octal literal");
+  }
+
+  return {
+    type: 'NumericLiteral',
+    value: parseNumeric(stream, idx, 8, start),
+    octal: false,
+    noctal: false
+  };
+}
+
+function readBinaryLiteral(stream) {
+  let start, idx = start = 2;
+  let char = stream.peek(idx);
+
+  while(!isEOS(char)) {
+    if (char !== "0" && char !== "1") {
+      break;
+    }
+    char = stream.peek(idx);
+    idx++;
+  }
+
+  if (idx === start) {
+    throw Error("Illegal binary literal");
+  }
+
+  if (!isEOS(char) && (isIdentifierStart(char) || isDecimalChar(char))) {
+    throw Error("Illegal binary literal");
+  }
+
+  return {
+    type: 'NumericLiteral',
+    value: parseNumeric(stream, idx, 2, start),
+    octal: false,
+    noctal: false
+  }
+}
+
+function readHexLiteral(stream) {
+  let start, char, idx = start = 2;
+  while(true) {
+    char = stream.peek(idx);
+    let hex = getHexValue(char);
+    if (hex === -1) {
+      break;
+    }
+    idx++;
+  }
+
+  if (idx === start) {
+    throw Error("Illegal hex literal");
+  }
+
+  if (!isEOS(char) && isIdentifierStart(char)) {
+    throw Error("Illegal hex literal");
+  }
+
+  return {
+    type: "NumericLiteral",
+    value: parseNumeric(stream, idx, 16, start)
+  }
+}
+
+function parseNumeric(stream, len, radix, start) {
+  if (start) stream.readString(start);
+  return parseInt(stream.readString(len), radix);
+}
+
+function isDecimalChar(char) {
+  return '0' <= char && char <= '9';
+}
--- test/test_default_readtable.js
+++ test/test_default_readtable.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import expect from 'expect.js';
 
 import CharStream from '../src/char-stream';
 import defaultReadtable from '../src/default-readtable';
@@ -11,74 +12,121 @@ function getEntryFor(stream) {
   return defaultReadtable.getEntry(key);
 }
 
-function testParseIdentifier(t, source, id) {
+function testParse(source, tst) {
   const stream = new CharStream(source);
-  const result = getEntryFor(stream).action(stream);
+  const entry = getEntryFor(stream);
+  const result = entry.action(stream);
 
-  t.is(result.value, id);
-  t.is(result.type, 'Identifier');
-  t.deepEqual(result.locationInfo, {
-    filename: '',
-    line: 0,
-    column: 0,
-    position: 0
-  });
+  tst(result, stream);
 }
 
 test('should parse Unicode identifiers', t => {
+  function testParseIdentifier(t, source, id) {
+    testParse(source, result => {
+      t.is(result.value, id);
+      t.is(result.type, 'Identifier');
+      t.deepEqual(result.locationInfo, {
+        filename: '',
+        line: 0,
+        column: 0,
+        position: 0
+      });
+    });
+  }
+
   testParseIdentifier(t, 'abcd ', 'abcd');
   testParseIdentifier(t, '日本語 ', '日本語');
-  testParseIdentifier(t, '\\u2163\\u2161 ', '\u2163\u2161');
-  testParseIdentifier(t, '\\u{102A7} ', '\u{102A7}');
+  testParseIdentifier(t, '\u2163\u2161 ', '\u2163\u2161');
+  testParseIdentifier(t, '\u{102A7} ', '\u{102A7}');
   testParseIdentifier(t, '\uD800\uDC00 ', '\uD800\uDC00');
   testParseIdentifier(t, '\u2163\u2161\u200A', '\u2163\u2161');
 });
 
-function testParsePunctuator(t, source, p) {
-  const stream = new CharStream(source);
-  const result = getEntryFor(stream).action(stream);
-
-  t.is(result.value, p);
-  t.is(result.type, 'Punctuator');
-  t.deepEqual(result.locationInfo, {
-    filename: '',
-    line: 0,
-    column: 0,
-    position: 0
-  });
-}
-
 test('should parse punctuators', t => {
+  function testParsePunctuator(t, source, p) {
+    testParse(source, result => {
+      t.is(result.value, p);
+      t.is(result.type, 'Punctuator');
+      t.deepEqual(result.locationInfo, {
+        filename: '',
+        line: 0,
+        column: 0,
+        position: 0
+      });
+    });
+  }
+
   testParsePunctuator(t, '; ', ';');
   testParsePunctuator(t, '>>> ', '>>>');
 });
 
-function testParseWhiteSpace(t, source) {
-  const stream = new CharStream(source);
-  const result = getEntryFor(stream).action(stream);
-
-  t.is(result, undefined);
-}
-
 test('should parse whitespace', t => {
+  function testParseWhiteSpace(t, source) {
+    testParse(source, result => t.is(result, undefined));
+  }
   testParseWhiteSpace(t, ' ');
   testParseWhiteSpace(t, '\t');
   testParseWhiteSpace(t, '\uFEFF');
 });
 
-function testParseLineTerminators(t, source) {
-  const stream = new CharStream(source);
-  const result = getEntryFor(stream).action(stream);
-  const { line, column, position } = stream.locationInfo;
-
-  t.is(result, undefined);
-  t.is(line, 1);
-  t.is(column, 0);
-  t.is(position, 1);
-}
-
 test('should parse line terminators', t => {
+  function testParseLineTerminators(t, source) {
+    testParse(source, (result, stream) => {
+      const { line, column, position } = stream.locationInfo;
+      t.is(result, undefined);
+      t.is(line, 1);
+      t.is(column, 0);
+      t.is(position, 1);
+    });
+  }
   testParseLineTerminators(t, '\n');
   testParseLineTerminators(t, '\r\n');
   testParseLineTerminators(t, '\u2029');
+});
+
+test('should parse numeric literals', t => {
+  function testParseNumericLiterals(t, source, value) {
+    testParse(source, result => t.is(result.value, value));
+  }
+  testParseNumericLiterals(t, '0xFFFF ', 0xFFFF);
+  testParseNumericLiterals(t, '0xFF ', 0xFF);
+  testParseNumericLiterals(t, '0o0756 ', 0o0756);
+  testParseNumericLiterals(t, '0o76 ', 0o76);
+  testParseNumericLiterals(t, '0b1010 ', 0b1010);
+  testParseNumericLiterals(t, '0b10 ', 0b10);
+  testParseNumericLiterals(t, '042 ', 0o042);
+  testParseNumericLiterals(t, '42 ', 42);
+});
+
+test('should parse string literals', t => {
+  function testParseStringLiteral(t, source, value) {
+    testParse(source, result => /*t.is(result.value, value)*/expect(result.value).to.eql(value));
+  }
+  testParseStringLiteral(t, '""', '');
+  testParseStringLiteral(t, "'x'", 'x');
+  testParseStringLiteral(t, '"x"', 'x');
+  testParseStringLiteral(t, "'\\\\\\''", "\\'");
+  testParseStringLiteral(t, '"\\\\\\\""', '\\\"');
+  testParseStringLiteral(t, "'\\\r'", '\r');
+  testParseStringLiteral(t, '"\\\r\n"', '\r\n');
+  testParseStringLiteral(t, '"\\\n"', '\n');
+  testParseStringLiteral(t, '"\\\u2028"', '\u2028');
+  testParseStringLiteral(t, '"\\\u2029"', '\u2029');
+  testParseStringLiteral(t, '"\u202a"', '\u202a');
+  testParseStringLiteral(t, '"\\0"', '\0');
+  testParseStringLiteral(t, '"\\0x"', '\0x');
+  testParseStringLiteral(t, '"\\01"', '\x01');
+  testParseStringLiteral(t, '"\\1"', '\x01');
+  testParseStringLiteral(t, '"\\11"', '\t');
+  testParseStringLiteral(t, '"\\111"', 'I');
+  testParseStringLiteral(t, '"\\1111"', 'I1');
+  testParseStringLiteral(t, '"\\2111"', '\x891');
+  testParseStringLiteral(t, '"\\5111"', ')11');
+  testParseStringLiteral(t, '"\\5a"', '\x05a');
+  testParseStringLiteral(t, '"\\7a"', '\x07a');
+  testParseStringLiteral(t, '"\a"', 'a');
+  testParseStringLiteral(t, '"\\u{00F8}"', '\xF8');
+  testParseStringLiteral(t, '"\\u{0}"', '\0');
+  testParseStringLiteral(t, '"\\u{10FFFF}"', '\uDBFF\uDFFF');
+  testParseStringLiteral(t, '"\\u{0000000000F8}"', '\xF8');
 });
