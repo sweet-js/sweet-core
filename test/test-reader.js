17d4a9b8095fc89a61c721b9d556d343ae818b59
Tim Disney <tim.disney@gmail.com>
2017-02-16 22:39:45 -0800

    Use number of backslashes

--- test/test-reader.js
+++ test/test-reader.js
@@ -116,12 +116,12 @@ test('should handle nested syntax templates', t => {
 });
 
 test.skip('should handle escaped string templates literals inside a syntax literal', t => {
-  let [r] = read('#`x = \`foo\``');
+  let [r] = read('#`x = \\`foo\\``');
   t.true(T.isSyntaxTemplate(r));
   let [open, x, eq, str, close] = r;
   t.true(T.isIdentifier(x, 'x'));
   t.true(T.isPunctuator(eq, '='));
-  t.true(T.isString(str, 'foo'));
+  t.true(T.isTemplate(str, 'foo'));
 });
 
 test('should read a regex when it begins the source', t => {
