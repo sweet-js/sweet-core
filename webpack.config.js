76dc15a9179407a88bd4e3d6a3036fde7bbe2a4e
Tim Disney <tim.disney@gmail.com>
2017-04-22 22:49:30 -0700

    Extract website

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	entry: './build/src/browser-sweet.js',
-	output: {
-		path: __dirname + '/browser/scripts',
-		library: "sweet",
-		libraryTarget: 'amd',
-		filename: 'sweet.js'
-	}
-};
